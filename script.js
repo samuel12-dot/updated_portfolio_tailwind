261a242635436704ec2537189317269632a44ef6
samuel12-dot <samuelogunduyile76@gmail.com>
2025-08-10 14:47:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const mobileMenu = document.querySelector("#mobileMenu")
+const navBar = document.querySelector("nav")
+const navLinks = document.querySelector("nav ul")
+
+openMenu = () => {
+    mobileMenu.style.transform = 'translateX(-16rem)'
+}
+closeMenu = () => {
+    mobileMenu.style.transform = 'translateX(16rem)'
+}
+window.addEventListener("scroll", () => {
+    if (scrollY > 50) {
+        navBar.classList.add("bg-white", "opacity-50", "backdrop-blur-lg", "shadow-sm")
+        navLinks.classList.remove("bg-white", "opacity-50", "shadow-sm")
+    }else {
+        navBar.classList.remove("bg-white", "opacity-50", "backdrop-blur-lg", "shadow-sm")
+        navLinks.classList.add("bg-white", "opacity-50", "shadow-sm")
+    }
+})
